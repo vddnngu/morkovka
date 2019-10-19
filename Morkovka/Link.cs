9f468b421aa8d4da1acfbc6a8cf37ee19e86f7f5
dvolkov <vdddzer152@yandex.ru>
2019-10-19 21:58:48 +0300

    Create Link.cs and added classes

---
+++ Morkovka/Link.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morkovka
+{
+
+    abstract class Link
+    {
+        string Text;
+        public abstract bool isQuestion();
+        public string getText()
+        {
+            return Text;
+        }
+    }
+    class Question : Link
+    {
+        bool isQuest;
+        Dictionary<String, Link> map;
+        public override bool isQuestion()
+        {
+            return isQuest;
+        }
+        public Link getNext(String answer)
+        {
+            return map[answer];
+        }
+    }
+}
