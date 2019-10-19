afb2f49b9cce05dcc216cca6e84af4084f10610e
Kataurovaa <kataurovas@icloud.com>
2019-10-19 21:16:40 +0300

    Добавила кнопку

--- Morkovka/Form1.cs
+++ Morkovka/Form1.cs
@@ -16,5 +16,10 @@ namespace Morkovka
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Hello";
+        }
     }
 }
