905efe42fcc349be3b278f35bb275c9c59ee5966
dvolkov <vdddzer152@yandex.ru>
2019-10-19 21:42:21 +0300

    Return to the Last Version

--- Morkovka/Form1.cs
+++ Morkovka/Form1.cs
@@ -16,10 +16,9 @@ namespace Morkovka
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            label1.Text = "Hello";
+
         }
     }
 }
