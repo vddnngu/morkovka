52f25c8925e50a28868f6e4413ba7952d978e2e4
dvolkov <vdddzer152@yandex.ru>
2019-10-23 21:19:13 +0300

    Добавил класс TestProcessing

---
+++ Morkovka/TestProcessing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morkovka
+{
+    class TestProcessing
+    {
+        Link currentLink;
+        public List<String> getAnswers()
+        {
+            if (currentLink.GetType() == typeof(Question))
+                return (currentLink as Question).getAnswers();
+            else
+                return null;
+        }
+        public bool goNext(String answer)
+        {
+            currentLink = (currentLink as Question).getNext(answer);
+            return currentLink.isQuestion();
+        }
+    }
+}
