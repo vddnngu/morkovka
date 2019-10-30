d72ee3f523efc397d1f1aa2011f262a60360c2f2
Kataurovaa <kataurovas@icloud.com>
2019-10-30 21:22:26 +0300

    init commit TestCase3010

--- Morkovka/Form1.Designer.cs
+++ Morkovka/Form1.Designer.cs
@@ -28,13 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(66, 79);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(232, 48);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(66, 179);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(232, 113);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(346, 179);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(232, 55);
+            this.richTextBox3.TabIndex = 2;
+            this.richTextBox3.Text = "";
+            this.richTextBox3.TextChanged += new System.EventHandler(this.richTextBox3_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 254);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 38);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ввести";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +89,11 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Morkovka/Form1.cs
+++ Morkovka/Form1.cs
@@ -12,11 +12,78 @@ namespace Morkovka
 {
     public partial class Form1 : Form
     {
+        TestProcessing GameTest;
+
         public Form1()
         {
             InitializeComponent();
+            Link root = FillQuestions();
+            GameTest = new TestProcessing(root);
+            var answers = GameTest.getAnswers();
+            foreach (var a in answers) { richTextBox2.Text += "\n" + a; }
         }
+
+        private Link FillQuestions()
+        {
+            Answer a1, a2, a3;
+            Question q1, q2;
+            List<String> str = new List<String>();
+            List<Link> lnk = new List<Link>();
+
+
+            a1 = new Answer("A1");
+            a2 = new Answer("A2");
+            a3 = new Answer("A3");
+
+
+            str.Add("a1");
+            str.Add("a2");
+            lnk.Add(a1);
+            lnk.Add(a2);
+            q1 = new Question("q1", str, lnk);
+
+
+            str.Clear();
+            lnk.Clear();
+
+
+            str.Add("a3");
+            str.Add("q1");
+            lnk.Add(a3);
+            lnk.Add(q1);
+            q2 = new Question("q2", str, lnk);
+
+            return q2;
+        }
+
         private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+            richTextBox2.Text = "";
+
+            GameTest.goNext(richTextBox3.Text);
+            var answers = GameTest.getAnswers();
+            
+            foreach (var a in answers)
+            {
+                richTextBox2.Text += "\n" + a;
+            }
+            //TODO: возможность в гейм тестинге получить доступ к текущему линку 
+            
+            richTextBox3.Text = "";
+        }
+
+        private void richTextBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
